e4aacdfc438f5aea30cbc299adcdee66c573697b
Heng Lin <enjoymycodinglife@gmail.com>
2018-05-21 23:16:45 +0800

    add MedianOfTwoSortedArrays algo

--- src/main/java/pers/henglin/leetcode/array/MedianOfTwoSortedArrays.java
+++ src/main/java/pers/henglin/leetcode/array/MedianOfTwoSortedArrays.java
@@ -1,7 +1,47 @@
 package pers.henglin.leetcode.array;
 
+import com.google.gson.Gson;
+import pers.henglin.leetcode.Verification;
+
 /**
  * Created by linheng on 2018/5/21.
  */
-public class MedianOfTwoSortedArrays {
+public class MedianOfTwoSortedArrays implements Verification<Integer[], Double> {
+    private Double medianOfTwoSortedArrays(Integer[] A, Integer[] B){
+        int m = A.length;
+        int n = B.length;
+        if(m > n){// to ensure m<=n
+            Integer[] temp = A; A = B; B = temp;
+            Integer tmp = m; m = n; n = tmp;
+        }
+        int iMin = 0, iMax = m, halfLen = (m + n + 1) / 2;
+        while (iMin <= iMax) {
+            int i = (iMin + iMax) / 2;
+            int j = halfLen - i;
+            if (i < iMax && B[j-1] > A[i]){
+                iMin = iMin + 1; // i is too small
+            }else if (i > iMin && A[i-1] > B[j]) {
+                iMax = iMax - 1; // i is too big
+            }else{// i is perfect
+                double maxLeft = 0;
+                if (i == 0) { maxLeft = B[j-1]; }
+                else if (j == 0) { maxLeft = A[i-1]; }
+                else { maxLeft = Math.max(A[i-1], B[j-1]); }
+                if ( (m + n) % 2 == 1 ) { return maxLeft; }
+
+                int minRight = 0;
+                if (i == m) { minRight = B[j]; }
+                else if (j == n) { minRight = A[i]; }
+                else { minRight = Math.min(B[j], A[i]); }
+
+                return (maxLeft + minRight) / 2.0;
+            }
+        }
+        return 0.0;
+    }
+
+    @Override
+    public Double verify(Integer[]... input) {
+        return medianOfTwoSortedArrays(input[0], input[1]);
+    }
 }
--- src/test/groovy/pers/henglin/leetcode/array/MedianOfTwoSortedArraysTest.groovy
+++ src/test/groovy/pers/henglin/leetcode/array/MedianOfTwoSortedArraysTest.groovy
@@ -1,7 +1,23 @@
 package pers.henglin.leetcode.array
 
+import pers.henglin.leetcode.Verification
+import spock.lang.Specification
+
 /**
  * Created by linheng on 2018/5/21.
  */
-class MedianOfTwoSortedArraysTest {
+class MedianOfTwoSortedArraysTest extends Specification{
+    def "test MedianOfTwoSortedArraysTest"(){
+        setup: "setup MedianOfTwoSortedArraysTest"
+        Verification medianOfTwoSortedArrays = new MedianOfTwoSortedArrays()
+
+        expect: "verify result"
+        medianOfTwoSortedArrays.verify(nums1 as Integer[] , nums2 as Integer[]) == output as Double
+
+        where: "input data set"
+        nums1  | nums2  || output
+        [1, 3] | [2]    || 2.0
+        [1, 2] | [3, 4] || 2.5
+
+    }
 }
