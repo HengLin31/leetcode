96d3cd3ad351712acc473c8a6a11f6bb25d03377
Heng Lin <enjoymycodinglife@gmail.com>
2018-06-07 23:08:37 +0800

    code typesetting

--- src/main/java/pers/henglin/leetcode/LeetcodeList.java
+++ src/main/java/pers/henglin/leetcode/LeetcodeList.java
@@ -3,22 +3,23 @@ package pers.henglin.leetcode;
 import pers.henglin.leetcode.array.*;
 import pers.henglin.leetcode.dp.*;
 import pers.henglin.leetcode.linkedlist.*;
+import pers.henglin.leetcode.math.PalindromeNumber;
 import pers.henglin.leetcode.string.*;
 
 public class LeetcodeList {
     private void questionNumber(){
-        TwoSum no001;
-        AddTwoNumbers no002;
-        LongestSubstringWithoutRepeatingCharacters no003;
-        MedianOfTwoSortedArrays no004;
-        LongestPalindromicSubstring no005;
-        ZigZagConversion no006;
-        ReverseInteger no007;
-        StringToInteger no008;
-        //PalindromeNumber no009;
-        RegularExpressionMatching no010;
-        ContainerWithMostWater no011;
-        IntegerToRoman no012;
-        RomanToInteger no013;
+        TwoSum                                      no001;
+        AddTwoNumbers                               no002;
+        LongestSubstringWithoutRepeatingCharacters  no003;
+        MedianOfTwoSortedArrays                     no004;
+        LongestPalindromicSubstring                 no005;
+        ZigZagConversion                            no006;
+        ReverseInteger                              no007;
+        StringToInteger                             no008;
+        PalindromeNumber                            no009;
+        RegularExpressionMatching                   no010;
+        ContainerWithMostWater                      no011;
+        IntegerToRoman                              no012;
+        RomanToInteger                              no013;
     }
 }
